37f629f1002f7f9a9d7ceb8ddfbef0570b27132a
Craig Reese <reeseca90@gmail.com>
2026-07-06 19:33:56 -0500

    updated to a log file instead of system out

--- src/main/java/com/craigreesedev/demo/aspect/MonitorExecutionTime.java
+++ src/main/java/com/craigreesedev/demo/aspect/MonitorExecutionTime.java
@@ -1,5 +1,8 @@
 package com.craigreesedev.demo.aspect;
 
+import java.io.FileWriter;
+import java.nio.file.Paths;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -8,14 +11,21 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MonitorExecutionTime {
+    final static String logFilePath = Paths.get("").toAbsolutePath().toString() + "/log";
+
     @Around("execution(* com.craigreesedev.demo.user.UserController.*(..))")
     public Object monitorExecutionTime(ProceedingJoinPoint pjp) throws Throwable {
         long startTime = System.currentTimeMillis();
         Object result = pjp.proceed();
         long endTime = System.currentTimeMillis();
 
-        // TODO: output this in a log file
-        System.out.println("Execution time (ms): %d".formatted(endTime - startTime));
+        // TODO: update this to be a CSV-style log file with timestamp, method name, execution time
+        try (FileWriter output = new FileWriter(logFilePath)) {
+            output.write("Execution time (ms): %d".formatted(endTime - startTime));
+        } catch (Exception e) {
+            System.out.println("Error writing to log file");
+        }
+
         return result;
     }
 }
